a5ffc0c7d1060bae91576a6113037021936e644e
Noe Martin <nmartin.contactpro@gmail.com>
2026-09-19 21:14:07 +0200

    feat(curl): curl installation added in server.sh k3s installation script

--- p1/server.sh
+++ p1/server.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+apt-get updates
+apt-get install -y curl
+
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip 192.168.56.110" sh -
 
 while [ ! -f /var/lib/rancher/k3s/server/node-token ]; do
