95e740f2fef06741bc2bc54e59f2da02a2fc9ac4
Marwan <maraisscode@gmail.com>
2026-09-20 13:49:53 +0200

    feat(scripts/yaml) : added scripts, yaml for app conf, k3s and kubectl working so far

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -1,1 +1,22 @@
-//on installe tout ce qui faut dans la vm
+#!/bin/bash
+# //on installe tout ce qui faut dans la vm
+set -e
+
+export INSTALL_K3S_EXEC="server \
+  --node-ip=192.168.56.110 \
+  --bind-address=192.168.56.110 \
+  --advertise-address=192.168.56.110 \
+  --tls-san=192.168.56.110"
+
+curl -sfL https://get.k3s.io | sh -
+
+mkdir -p /home/vagrant/.kube
+cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+chown vagrant:vagrant /home/vagrant/.kube/config
+echo "export KUBECONFIG=/home/vagrant/.kube/config" >> /home/vagrant/.bashrc
+
+until kubectl get nodes 2>/dev/null | grep -q " Ready "; do
+  sleep 3
+done
+
+kubectl apply -f /vagrant_confs/
