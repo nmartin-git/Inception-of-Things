b95ca385eb187ed048ed48fad4784e192ee4e471
Noe Martin <nmartin.contactpro@gmail.com>
2026-09-20 17:54:01 +0200

    fix(worker): optimized k3s server installation according to low ressources, fixed typo error in Vagrantfile and add specific ip according to static ip in worker script

--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -5,4 +5,4 @@ set -e
 apt-get update
 apt-get install -y curl
 
-curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN_FILE="/vagrant/node-token" sh -
+curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN_FILE="/vagrant/node-token" INSTALL_K3S_EXEC="agent --node-ip 192.168.56.111" sh -
