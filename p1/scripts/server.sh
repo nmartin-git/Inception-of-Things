86d08f9c659dea6c920ac8373dc6f09d540372f0
Noe Martin <nmartin.contactpro@gmail.com>
2026-09-20 16:59:20 +0200

    feat(worker): add worker server k3s installation script and add automatic launch in Vagrantfile

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-apt-get updates
+apt-get update
 apt-get install -y curl
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip 192.168.56.110" sh -
---
+++ p1/scripts/worker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+apt-get update
+apt-get install -y curl
+
+curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN_FILE="/vagrant/node-token" sh -
